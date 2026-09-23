aec1bce9e2d0b0e3a3b943fe48162df0f83b205c
Nidhi Mishra <nidhiimishraa20@gmail.com>
2026-09-23 15:11:22 +0530

    Add attempt counter to guessing game

--- python-basics/guessing_game.py
+++ python-basics/guessing_game.py
@@ -1,14 +1,17 @@
 import random
 
 number = random.randint(1, 10)
+attempts = 0
 
 while True:
     guess = int(input("Guess a number between 1 and 10: "))
+    attempts += 1
 
     if guess == number:
         print("🎉 Correct! You guessed it!")
+        print("You got it in", attempts, "attempts!")
         break
     elif guess < number:
-    print("📈 Try a higher number!")
-else:
-    print("📉 Try a lower number!")
+        print("📈 Try a higher number!")
+    else:
+        print("📉 Try a lower number!")
